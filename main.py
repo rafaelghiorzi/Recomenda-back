4d806b0185028656964c5b0768bac1fbc28b6ac9
Rafael Ghiorzi <rafael.ghiorzi@gmail.com>
2024-12-06 03:42:50 -0300

    batman

---
+++ main.py
@@ -0,0 +1,11 @@
+".venv/Scripts/activate"
+"fastapi dev main.py"
+
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
